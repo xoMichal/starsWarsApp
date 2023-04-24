96b60427c8792dcc6407d771127e0c14bf9c70d7
xoMichal <michalrusek198@gmail.com>
2023-04-24 22:18:26 +0200

    'feat-1-add-logic-and-ui-material'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  Button,
+  Grid,
+  InputBase,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import { Search as SearchIcon } from '@material-ui/icons';
+import { useStyles } from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Character {
+  name: string;
+  homeworld: string;
+  homeworldPopulation: string;
+  films: string[];
 }
 
-export default App;
+interface Movie {
+  title: string;
+  releaseDate: string;
+  openingCrawl: string;
+}
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
+  const [query, setQuery] = useState('');
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSearch = async () => {
+    const response = await fetch(`https://swapi.dev/api/people/?search=${query}`);
+    const data = await response.json();
+    const results = data.results as Character[];
+    setCharacters(results);
+  };
+
+  const handleCharacterClick = async (character: Character) => {
+    const response = await fetch(character.homeworld);
+    const data = await response.json();
+    const homeworldPopulation = data.population;
+    const movieUrls = character.films as string[];
+    const movieResponses = await Promise.all(movieUrls.map(url => fetch(url)));
+    const movieData = await Promise.all(movieResponses.map(response => response.json()));
+    const movies = movieData.map(movie => ({
+      title: movie.title,
+      releaseDate: movie.release_date,
+      openingCrawl: movie.opening_crawl.substring(0, 130),
+    }));
+    setMovies(movies);
+  };
+
+    return (
+      <Grid container spacing={2} className={classes.container}>
+        <Grid item xs={10}>
+          <Paper elevation={2} className={classes.searchContainer}>
+            <InputBase
+              placeholder="Search for Star Wars characters"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              className={classes.searchInput}
+            />
+          <Button variant="text" color="inherit" startIcon={<SearchIcon />} onClick={handleSearch}>
+              Search
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} className={classes.characterListContainer}>
+            <Typography variant="h5" component="h2" gutterBottom className={classes.sectionTitle}>
+              Characters
+            </Typography>
+            <List>
+              {characters.map(character => (
+                <ListItem key={character.name} button onClick={() => handleCharacterClick(character)}>
+                  <ListItemText
+                    primary={character.name}
+                    secondary={`${character.homeworld}, population ${character.homeworldPopulation ? character.homeworldPopulation : "neutral" }`}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} className={classes.movieListContainer}>
+            <Typography variant="h5" component="h2" gutterBottom className={classes.sectionTitle}>
+            Movies
+            </Typography>
+            <List>
+              {movies.map(movie => (
+                <ListItem key={movie.title}>
+                  <ListItemText
+                    primary={`${movie.title} (${movie.releaseDate})`}
+                    secondary={movie.openingCrawl}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
+  
+  export default App
---
+++ src/styles.ts
@@ -0,0 +1,44 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles(theme => ({
+  searchContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(2),
+  },
+  searchInput: {
+    marginRight: theme.spacing(1),
+    width: '80%',
+    paddingLeft:'16px'
+    
+  },
+  searchButton: {
+    minWidth: 'unset',
+  },
+  characterListContainer: {
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    borderRight: `1px solid ${theme.palette.divider}`,
+    [theme.breakpoints.down('sm')]: {
+      paddingRight: 0,
+      borderRight: 'none',
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+  },
+  movieListContainer: {
+    paddingLeft: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: 0,
+    },
+  },
+  sectionTitle: {
+    marginBottom: theme.spacing(2),
+  },
+  container: {
+    maxWidth: '1600px',
+    margin: '0px',
+    padding: '16px',
+    justifyContent:'center'
+
+  },
+}));
